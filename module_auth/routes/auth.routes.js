d878fc5ca3242c6742996c2b83bdcc34505fd4c3
VenketeshRushi <rushivenketesh@gmail.com>
2023-09-06 19:10:52 +0530

    updated google auth route

--- module_auth/routes/auth.routes.js
+++ module_auth/routes/auth.routes.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 
 const handleErrors = require("../middleware/handleErrors");
 const authorization = require("../middleware/authorization");
 const UserController = require("../controllers/UserController");
+
+router.use(passport.initialize());
+router.use(passport.session());
 
 // HANDLING ERRORS
 router.use(handleErrors);
@@ -22,5 +26,33 @@ router.post("/resetpassword", UserController.resetPassword);
 
 // UPDATE USER PROFILE
 // router.put("/profile", authorization, UserController.updateProfile);
+
+// router.get(
+//   "/auth/google",
+//   passport.authenticate("google", { scope: ["profile", "email"] })
+// );
+
+// router.get(
+//   "/auth/google/callback",
+//   passport.authenticate("google", { failureRedirect: "/fail" }),
+//   function (req, res) {
+//     res.redirect(`http://localhost:5173/ecom-client`);
+//   }
+// );
+
+// router.get("/fail", (req, res) => {
+//   res.redirect(`http://localhost:5173/ecom-client/login`);
+// });
+
+router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/");
+  }
+);
 
 module.exports = router;
