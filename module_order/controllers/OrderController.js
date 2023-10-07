86df1294c6ea2dc95307be9a8f57079eed87820d
VenketeshRushi <rushivenketesh@gmail.com>
2023-10-07 10:35:40 +0530

    fixing order route bugs

--- module_order/controllers/OrderController.js
+++ module_order/controllers/OrderController.js
@@ -23,37 +23,42 @@ exports.createOrder = async (req, res, next) => {
   } = req.body;
   try {
     // Create related records first, assuming they are in the correct shape in req.body
-    const createdShippingDetails = await prisma.shippingDetail.create({
-      data: { ...shippingDetails },
-    });
+    // const createdShippingDetails = await prisma.shippingDetail.create({
+    //   data: { ...shippingDetails },
+    // });
 
-    const createdPaymentDetails = await prisma.paymentDetail.create({
-      data: { ...paymentDetails },
-    });
+    // const createdPaymentDetails = await prisma.paymentDetail.create({
+    //   data: { ...paymentDetails },
+    // });
 
     // const createdCartProducts = await prisma.cartProduct.createMany({
     //   data: cartProducts,
     // });
 
     // Create the order record with references to the related records
+    // const order = await prisma.order.create({
+    //   data: {
+    //     subTotal,
+    //     quantity,
+    //     shipping,
+    //     discount,
+    //     total,
+    //     userId: req.user.id,
+    //     ShippingDetails: {
+    //       connect: { id: createdShippingDetails.id },
+    //     },
+    //     PaymentDetails: {
+    //       connect: { id: createdPaymentDetails.id },
+    //     },
+    //     cartProducts: {
+    //       connect: createdCartProducts.map((product) => ({ id: product.id })),
+    //     },
+    //   },
+    // });
+
     const order = await prisma.order.create({
-      data: {
-        subTotal,
-        quantity,
-        shipping,
-        discount,
-        total,
-        userId: req.user.id,
-        ShippingDetails: {
-          connect: { id: createdShippingDetails.id },
-        },
-        PaymentDetails: {
-          connect: { id: createdPaymentDetails.id },
-        },
-        cartProducts: {
-          connect: createdCartProducts.map((product) => ({ id: product.id })),
-        },
-      },
+      ...req.body,
+      user: req.user.id,
     });
 
     return res.status(201).json(order);
